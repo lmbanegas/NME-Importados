04369393856eb0148247bc91952d1fe3a5affacb
desarrolloslbanegas <desarrolloslbanegas@gmail.com>
2025-10-01 22:39:38 -0300

    V2

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// const serverless = require("serverless-http");
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // --- Configuración de vistas ---
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// // --- Middlewares ---
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, '/public')));
+
+// // --- Rutas ---
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // --- Exportar como función serverless ---
+// module.exports = app;
+
+
+///// Prod
+
+// app.js
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -10,20 +43,27 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // --- Configuración de vistas ---
-app.set('views', path.join(__dirname, '..', 'views'));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// asumimos que la carpeta 'views' está en la raíz del proyecto, al mismo nivel que 'functions'
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // --- Middlewares ---
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
+
+// Para Netlify, express.static debe apuntar a la carpeta pública correcta
+app.use(express.static(path.join(__dirname, 'public')));
 
 // --- Rutas ---
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// --- Exportar como función serverless ---
-module.exports = app;
+// --- Manejo de errores 404 ---
+app.use(function(req, res, next) {
+  res.status(404).send('Página no encontrada');
+});
+
+// --- Exportar handler para Netlify ---
 module.exports.handler = serverless(app);
