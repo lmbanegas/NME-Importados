6836a4bef52da1a3d2967604a276e1575015266c
desarrolloslbanegas <desarrolloslbanegas@gmail.com>
2025-10-01 22:17:22 -0300

    Actualizacion

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const serverless = require("serverless-http");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('view engine', 'jade');
+// --- Configuración de vistas ---
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// --- Middlewares ---
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-
+// --- Rutas ---
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.set('view engine', 'ejs');
-
-app.set('views', [
-  path.join(__dirname, './views'),
-]);
-
-
-
+// --- Exportar como función serverless ---
 module.exports = app;
+module.exports.handler = serverless(app);
