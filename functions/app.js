24091c6ac851676a8fff563c8f5763b0b6add487
desarrolloslbanegas <desarrolloslbanegas@gmail.com>
2025-10-01 22:43:58 -0300

    V2

--- functions/app.js
+++ functions/app.js
@@ -42,28 +42,25 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// --- Configuración de vistas ---
-// asumimos que la carpeta 'views' está en la raíz del proyecto, al mismo nivel que 'functions'
-app.set('views', path.join(__dirname, 'views'));
+// Vistas
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
-// --- Middlewares ---
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
-// Para Netlify, express.static debe apuntar a la carpeta pública correcta
-app.use(express.static(path.join(__dirname, 'public')));
-
-// --- Rutas ---
+// Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// --- Manejo de errores 404 ---
-app.use(function(req, res, next) {
+// 404
+app.use((req, res, next) => {
   res.status(404).send('Página no encontrada');
 });
 
-// --- Exportar handler para Netlify ---
+// Exportar handler para Netlify
 module.exports.handler = serverless(app);
